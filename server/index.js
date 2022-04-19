2ac90cefdea2752bfad530831bb9617409631025
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-04-19 21:56:08 +0100

    connects to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 app.use(express.json());
 const PORT = 3000;
-
+const db = 'mongodb+srv://afolabi:8899fifafa@cluster0.bwrba.mongodb.net/eventsdb?retryWrites=true&w=majority';
+mongoose.connect(db, err => err ? console.log("ERROR: ", err) : console.log('Successfully connected to mongoDB'))
 const api = require('./routes/api');
 
 app.use('/api', api);
