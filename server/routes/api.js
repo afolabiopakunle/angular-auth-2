7d2f8e2c2a4292b52cbe27acbbb4095e6fab5414
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-04-19 22:14:49 +0100

    hitting the server and saving stuffs

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
     res.send('API Home')
+})
+
+router.post('/register', (req, res) => {
+    const userData = req.body;
+    const user = new User(userData);
+    user.save((err, registeredUser) => err ? console.log('ERROR', err) : res.status(200).send(registeredUser))
 })
 
 module.exports = router;
