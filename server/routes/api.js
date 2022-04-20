27db39ab9dae2f89d6149e68ccc67990891e1b52
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-04-20 08:49:21 +0100

    parked

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,5 +12,20 @@ router.post('/register', (req, res) => {
     user.save((err, registeredUser) => err ? console.log('ERROR', err) : res.status(200).send(registeredUser))
 })
 
+router.post('/login', (req, res) => {
+    const userData = req.body;
+    User.findOne({email: userData.email}, (err, foundUser) => {
+        if(err) {
+            console.log(err)
+        }
+        else if (!foundUser) {
+            res.status(401).res.send('Invalid email')
+        }
+        else if(userData.password !== foundUser.password) {
+            res.status(401).send("Invalid password")
+        } else res.status(200).send('Welcome ' + foundUser.email)
+    })
+})
+
 module.exports = router;
 
